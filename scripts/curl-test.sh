d1d0c886adf8e4d15d916eacd09c581c30a3f654
AIMMY-HOMEPC\noppo <noppon_aim@hotmail.com>
2022-08-23 19:59:59 +0700

    Add curl test. Change container name.

---
+++ scripts/curl-test.sh
@@ -0,0 +1,9 @@
+curl localhost:5600 --data-binary "@D:\DataLake\replay.dem"
+curl localhost:5600 --data-binary "@D:\DataLake\5979456813_913476658.dem"
+curl localhost:5600 --data-binary "@D:\DataLake\5979530001_349241717.dem"
+curl localhost:5600 --data-binary "@E:\Development\JetBrains\IdeaProjects\clarity-examples\src\main\resources\replay.dem"
+
+curl localhost:5600 --data-binary "@G:\My Drive\DataLake\replay.dem"
+curl localhost:5600 --data-binary  "@C:\Users\noppo\OneDrive - Chiang Mai University\DataLake\replay.dem"
+curl localhost:5600 --data-binary "@/home/noppon/Downloads/replay.dem"
+#java -jar target/combatlog.one-jar.jar replay.dem
